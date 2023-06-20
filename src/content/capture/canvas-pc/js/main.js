c83cd8e9ec5d8c7ea464bff5cf137c8d572eebf0
George Raed <graed@proximie.com>
2023-06-20 12:59:43 +0300

    updated canvas stream and added transceiver

--- src/content/capture/canvas-pc/js/main.js
+++ src/content/capture/canvas-pc/js/main.js
@@ -8,23 +8,27 @@
 
 /* global main */
 
-'use strict';
+"use strict";
 
-const canvas = document.querySelector('canvas');
-const video = document.querySelector('video');
+const canvas = document.querySelector("canvas");
+const video = document.querySelector("video");
 
 let pc1;
 let pc2;
 
 let startTime;
 
-video.addEventListener('loadedmetadata', function() {
-  document.getElementById('autoplay').style.display = 'none';
-  console.log(`Remote video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
+video.addEventListener("loadedmetadata", function () {
+  document.getElementById("autoplay").style.display = "none";
+  console.log(
+    `Remote video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`
+  );
 });
 
 video.onresize = () => {
-  console.log(`Remote video size changed to ${video.videoWidth}x${video.videoHeight}`);
+  console.log(
+    `Remote video size changed to ${video.videoWidth}x${video.videoHeight}`
+  );
   // We'll use the first onsize callback as an indication that video has started
   // playing out.
   if (startTime) {
@@ -38,12 +42,12 @@ video.onresize = () => {
 main();
 
 const stream = canvas.captureStream();
-console.log('Got stream from canvas');
+console.log("Got stream from canvas");
 
 call();
 
 function call() {
-  console.log('Starting call');
+  console.log("Starting call");
   startTime = window.performance.now();
   const videoTracks = stream.getVideoTracks();
   const audioTracks = stream.getAudioTracks();
@@ -55,26 +59,41 @@ function call() {
   }
   const servers = null;
   pc1 = new RTCPeerConnection(servers);
-  console.log('Created local peer connection object pc1');
-  pc1.onicecandidate = e => onIceCandidate(pc1, e);
+  console.log("Created local peer connection object pc1");
+  pc1.onicecandidate = (e) => onIceCandidate(pc1, e);
   pc2 = new RTCPeerConnection(servers);
-  console.log('Created remote peer connection object pc2');
-  pc2.onicecandidate = e => onIceCandidate(pc2, e);
-  pc1.oniceconnectionstatechange = e => onIceStateChange(pc1, e);
-  pc2.oniceconnectionstatechange = e => onIceStateChange(pc2, e);
+  console.log("Created remote peer connection object pc2");
+  pc2.onicecandidate = (e) => onIceCandidate(pc2, e);
+  pc1.oniceconnectionstatechange = (e) => onIceStateChange(pc1, e);
+  pc2.oniceconnectionstatechange = (e) => onIceStateChange(pc2, e);
   pc2.ontrack = gotRemoteStream;
 
-  stream.getTracks().forEach(
-      track => {
-        pc1.addTrack(
-            track,
-            stream
-        );
-      }
-  );
-  console.log('Added local stream to pc1');
+  // stream.getTracks().forEach(
+  //     track => {
+  //       pc1.addTrack(
+  //           track,
+  //           stream
+  //       );
+  //     }
 
-  console.log('pc1 createOffer start');
+  // );
+
+  stream.getTracks().forEach((track) => {
+    // pc1.addTrack(track, stream);
+    pc1.addTransceiver(videoTracks[0], {
+      direction: "sendonly",
+      streams: [stream],
+      sendEncodings: [
+        { rid: "h", maxBitrate: 1200 * 1024 },
+        { rid: "m", maxBitrate: 600 * 1024, scaleResolutionDownBy: 2 },
+        { rid: "l", maxBitrate: 300 * 1024, scaleResolutionDownBy: 4 },
+      ],
+    });
+  });
+
+  console.log("Added local stream to pc1");
+
+  console.log("pc1 createOffer start");
   pc1.createOffer(onCreateOfferSuccess, onCreateSessionDescriptionError);
 }
 
@@ -84,11 +103,19 @@ function onCreateSessionDescriptionError(error) {
 
 function onCreateOfferSuccess(desc) {
   console.log(`Offer from pc1\n${desc.sdp}`);
-  console.log('pc1 setLocalDescription start');
-  pc1.setLocalDescription(desc, () => onSetLocalSuccess(pc1), onSetSessionDescriptionError);
-  console.log('pc2 setRemoteDescription start');
-  pc2.setRemoteDescription(desc, () => onSetRemoteSuccess(pc2), onSetSessionDescriptionError);
-  console.log('pc2 createAnswer start');
+  console.log("pc1 setLocalDescription start");
+  pc1.setLocalDescription(
+    desc,
+    () => onSetLocalSuccess(pc1),
+    onSetSessionDescriptionError
+  );
+  console.log("pc2 setRemoteDescription start");
+  pc2.setRemoteDescription(
+    desc,
+    () => onSetRemoteSuccess(pc2),
+    onSetSessionDescriptionError
+  );
+  console.log("pc2 createAnswer start");
   // Since the 'remote' side has no media stream we need
   // to pass in the right constraints in order for it to
   // accept the incoming offer of audio and video.
@@ -110,28 +137,41 @@ function onSetSessionDescriptionError(error) {
 function gotRemoteStream(e) {
   if (video.srcObject !== e.streams[0]) {
     video.srcObject = e.streams[0];
-    console.log('pc2 received remote stream');
+    console.log("pc2 received remote stream");
     if (video.paused) {
-      document.getElementById('autoplay').style.display = 'block';
+      document.getElementById("autoplay").style.display = "block";
     }
   }
 }
 
 function onCreateAnswerSuccess(desc) {
   console.log(`Answer from pc2:\n${desc.sdp}`);
-  console.log('pc2 setLocalDescription start');
-  pc2.setLocalDescription(desc, () => onSetLocalSuccess(pc2), onSetSessionDescriptionError);
-  console.log('pc1 setRemoteDescription start');
-  pc1.setRemoteDescription(desc, () => onSetRemoteSuccess(pc1), onSetSessionDescriptionError);
+  console.log("pc2 setLocalDescription start");
+  pc2.setLocalDescription(
+    desc,
+    () => onSetLocalSuccess(pc2),
+    onSetSessionDescriptionError
+  );
+  console.log("pc1 setRemoteDescription start");
+  pc1.setRemoteDescription(
+    desc,
+    () => onSetRemoteSuccess(pc1),
+    onSetSessionDescriptionError
+  );
 }
 
 function onIceCandidate(pc, event) {
-  getOtherPc(pc).addIceCandidate(event.candidate)
-      .then(
-          () => onAddIceCandidateSuccess(pc),
-          err => onAddIceCandidateError(pc, err)
-      );
-  console.log(`${getName(pc)} ICE candidate: ${event.candidate ? event.candidate.candidate : '(null)'}`);
+  getOtherPc(pc)
+    .addIceCandidate(event.candidate)
+    .then(
+      () => onAddIceCandidateSuccess(pc),
+      (err) => onAddIceCandidateError(pc, err)
+    );
+  console.log(
+    `${getName(pc)} ICE candidate: ${
+      event.candidate ? event.candidate.candidate : "(null)"
+    }`
+  );
 }
 
 function onAddIceCandidateSuccess(pc) {
@@ -139,20 +179,22 @@ function onAddIceCandidateSuccess(pc) {
 }
 
 function onAddIceCandidateError(pc, error) {
-  console.log(`${getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
+  console.log(
+    `${getName(pc)} failed to add ICE Candidate: ${error.toString()}`
+  );
 }
 
 function onIceStateChange(pc, event) {
   if (pc) {
     console.log(`${getName(pc)} ICE state: ${pc.iceConnectionState}`);
-    console.log('ICE state change event: ', event);
+    console.log("ICE state change event: ", event);
   }
 }
 
 function getName(pc) {
-  return (pc === pc1) ? 'pc1' : 'pc2';
+  return pc === pc1 ? "pc1" : "pc2";
 }
 
 function getOtherPc(pc) {
-  return (pc === pc1) ? pc2 : pc1;
+  return pc === pc1 ? pc2 : pc1;
 }
